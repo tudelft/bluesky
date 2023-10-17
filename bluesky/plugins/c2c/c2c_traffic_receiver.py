088a653b9bd1308b404b6bec356fd3444eb439f7
Dennis van Wijngaarden <32736330+Dennis-Wijngaarden@users.noreply.github.com>
2023-10-17 16:05:38 +0200

    [c2c_traffic_receiver] Added basic traffic receiver and test plugin

---
+++ bluesky/plugins/c2c/c2c_traffic_receiver.py
@@ -0,0 +1,66 @@
+import bluesky as bs
+from bluesky.core import Entity, timed_function
+from bluesky import stack
+
+import paho.mqtt.client as mqtt
+import threading
+
+import numpy as np
+
+import json
+
+def init_plugin():
+    # Instantiate C2CTraffic entity
+    c2c_traffic_receiver = C2CTrafficReceiver()
+    # Configuration parameters
+    config = {
+        'plugin_name': 'C2C_TRAFFIC_RECEIVER',
+        'plugin_type': 'sim'
+    }
+    return config
+
+class C2CTrafficReceiver(Entity):
+    def __init__(self):
+        super().__init__()
+
+        self.lock = threading.Lock()
+
+        # Mqtt message buffer
+        self.mqtt_msg_buf = []
+
+        # Start mqtt client to read out control commands
+        self.mqtt_client = MQTTC2CTrafficReceiverClient(self)
+        self.mqtt_client.run()
+
+    def recv_mqtt(self, msg):
+        self.lock.acquire()
+        try:
+            print(msg.topic)
+            if msg.topic == 'daa/traffic': 
+                self.mqtt_msg_buf.append(json.loads(msg.payload))
+                print(json.loads(msg.payload))
+        finally:
+            self.lock.release()
+
+    @timed_function(dt=0.05)
+    def update_c2c_traffic(self):
+        # Read message buffer to read new messages and empty
+        self.mqtt_msg_buf = []
+        return
+
+class MQTTC2CTrafficReceiverClient(mqtt.Client):
+    def __init__(self, c2c_traffic_object):
+        super().__init__()
+        self.c2c_traffic_object = c2c_traffic_object
+
+    def run(self):
+        self.connect("localhost", 1883, 60)
+        self.subscribe("daa/traffic", 0)
+        rc = self.loop_start()
+        return rc
+
+    def on_message(self, mqttc, obj, msg):
+        self.c2c_traffic_object.recv_mqtt(msg)
+
+    def stop(self):
+        self.loop_stop()
---
+++ bluesky/plugins/c2c/c2c_traffic_receiver_test.py
@@ -0,0 +1,71 @@
+import bluesky as bs
+from bluesky.core import Entity, timed_function
+from bluesky import stack
+
+import paho.mqtt.client as mqtt
+import threading
+
+import numpy as np
+
+import json
+
+def init_plugin():
+    # Instantiate C2CTraffic entity
+    c2c_traffic_receiver_test = C2CTrafficReceiverTest()
+    # Configuration parameters
+    config = {
+        'plugin_name': 'C2C_TRAFFIC_RECEIVER_TEST',
+        'plugin_type': 'sim'
+    }
+    return config
+
+class C2CTrafficReceiverTest(Entity):
+    def __init__(self):
+        super().__init__()
+
+        self.lock = threading.Lock()
+
+        self.mqtt_client = MQTTC2CTrafficClient()
+        self.mqtt_client.run()
+
+        # Traffic variables of test object(s)
+        self.ac_id = ['test_1']
+        self.vn = [5.]
+        self.ve = [0.]
+        self.vd = [0.]
+        self.lat = [52.3]
+        self.lon = [4.7]
+        self.alt = [40.]
+
+    @timed_function(dt=0.1)
+    def test_c2c_traffic_receiver(self):
+        for i in range(len(self.ac_id)):
+            message_dict = {"ac_id" : self.ac_id[i],\
+                            "lat" : self.lat[i],\
+                            "lon" : self.lon[i],\
+                            "alt" : self.alt[i],\
+                            "vn" : self.vn[i],\
+                            "ve" : self.ve[i],\
+                            "vd" : self.vd[i]}
+            self.mqtt_client.publish('daa/traffic', json.dumps(message_dict))
+
+            # Update lat lon alt
+            self.lat[i] = self.lat[i]
+            self.lon[i] = self.lon[i]
+            self.alt[i] = self.alt[i]
+
+class MQTTC2CTrafficClient(mqtt.Client):
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        self.connect("localhost", 1883, 60)
+        rc = self.loop_start()
+        return rc
+
+    def on_message(self, mqttc, obj, msg):
+        return
+
+    def stop(self):
+        self.loop_stop()
+
