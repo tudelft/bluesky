43a7b67667fe66d09c67e9c7c1925ff95c97d4cc
NoahWe <noah.wechtler@hotmail.com>
2025-10-24 10:46:56 +0200

    send tres in unix time

--- bluesky/plugins/asas/ssd_drone.py
+++ bluesky/plugins/asas/ssd_drone.py
@@ -805,7 +805,7 @@ class SSD_Drone(ConflictResolution):
                     body['waypoint']['lat'] = int(lat_res * 10**7)
                     body['waypoint']['lon'] = int(lon_res * 10**7)
                     body['waypoint']['alt'] = int(alt_res * 10**3)
-                    body['tres'] = float(tres) 
+                    body['tres'] = int(time.time() + float(tres))
                     # Make sure vres is not NaN
                     body['vres'] = float(dist_res * nm / tres) if not np.isclose(tres, 0.0) else 0.0
 
